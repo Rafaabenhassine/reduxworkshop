19784dcbdb44bcb9962ad884d344c9debb68e228
Rafaabenhassine <rafaabenhassine1@gmail.com>
2023-09-15 10:56:40 +0100

    redux workshop

---
+++ src/Components/PanierList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Panier from './Panier'
+import { useSelector } from 'react-redux'
+
+const PanierList = () => {
+  const Products=useSelector((state)=>state.ListReducer.ListProduct)
+  var k=0
+    Products.map(prod=>k=k+(prod.price*prod.compter))
+    
+
+  return (
+    <div>
+      <h1>Total price:<span>{k}</span></h1>
+{Products.filter((el)=>el.panier==true).map((prod)=>(<Panier prod={prod} key={prod.id} />))}
+
+    </div>
+  )
+}
+
+export default PanierList
---
+++ src/Components/ProductList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import Product from './Product'
+import { Button, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Panier from './Panier'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const ProductList = () => {
+    const Products=useSelector((state)=>state.ListReducer.ListProduct)
+    var j=0
+    Products.map(prod=>prod.panier?j=j+1:j)
+    
+    
+  return (
+    <div>
+    <Link to={"/panier"}> <button>{j}<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="red" className="bi bi-basket2" viewBox="0 0 16 16">
+  <path d="M4 10a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 1 1 2 0v2a1 1 0 0 1-2 0v-2z"/>
+  <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-.623l-1.844 6.456a.75.75 0 0 1-.722.544H3.69a.75.75 0 0 1-.722-.544L1.123 8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.163 8l1.714 6h8.246l1.714-6H2.163z"/>
+</svg> </button></Link>
+      <Link to={"/AddProduct"} ><Button>Add New Product</Button></Link>
+      <Link to={"/Movielist"} ><Button>Movie List</Button></Link>
+      <Link to={"/TaskList"}><Button>Task Todo</Button></Link>
+      
+        {Products.map(Prod=><Product Prod={Prod} key={Prod.id}  />)}
+
+
+    </div>
+  )
+}
+
+export default ProductList
